cb4c57ec6be5ce0b8ba168ff0442f60ac207d620
CHutchnson <cheyennehutchinson@outlook.com>
2021-11-22 14:57:58 +0000

    updated code for entity class

--- src/main/java/com/qa/project/member/GymMember.java
+++ src/main/java/com/qa/project/member/GymMember.java
@@ -30,8 +30,59 @@ public class GymMember {
 	@Column
 	private String email;
 
-	
-	
-	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDate getDOB() {
+		return DOB;
+	}
+
+	public void setDOB(LocalDate dOB) {
+		DOB = dOB;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "GymMember [id=" + id + ", accountNumber=" + accountNumber + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", DOB=" + DOB + ", email=" + email + "]";
+	}
+
 	
 }
